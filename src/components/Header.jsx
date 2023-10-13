e464084e1ad6b54ed1b0ccc854e8eaf71d6a56e4
Anush79 <barbieworld1056.a@gmail.com>
2023-10-13 16:57:19 +0530

    Added Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 
-
+import GitHubIcon from '@mui/icons-material/GitHub';
+import StorageIcon from '@mui/icons-material/Storage';
 export default function Header(){
   return <header>
     <div className="logo">InVent</div>
@@ -8,6 +9,8 @@ export default function Header(){
       <NavLink to='/'>Home</NavLink>
       <NavLink to='/inventory'>Inventory</NavLink>
       <NavLink to='/sales'>Sales</NavLink>
+      <NavLink to='https://github.com/Anush79/Inventory_Management'><GitHubIcon/></NavLink>
+      <NavLink to='https://replit.com/@AnushkaJaiswal7/inventoryManagement'><StorageIcon/></NavLink>
 
     </nav>
   </header>
